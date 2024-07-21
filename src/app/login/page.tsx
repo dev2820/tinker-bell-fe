6f436230aa82e1527822e361960bf30b832fc4c2
rladydgn <39542757+rladydgn@users.noreply.github.com>
2024-07-21 12:50:34 +0900

    chore: 주석 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,7 @@ export default async function BlogPostPage({
 }) {
   return (
     <div className="flex items-center justify-center h-screen">
-      {/* 로컬 실행시 redirect_uri=http://localhost:8080/oauth/redirect 로 변경 */}
+      {/* 로컬 BE 실행시 redirect_uri=http://localhost:8080/oauth/redirect 로 변경 */}
       <Link href="https://kauth.kakao.com/oauth/authorize?client_id=a9e8beed55b5a4c283dc2a33e2024108&redirect_uri=http://13.125.195.239:8080/oauth/redirect&response_type=code">
         <Image
           src="/assets/images/kakao_login_medium_narrow.png"
