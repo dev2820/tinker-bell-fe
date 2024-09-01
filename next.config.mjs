af4fa481c0ff1dc460f105393292c56996fb0cf5
rladydgn <39542757+rladydgn@users.noreply.github.com>
2024-09-01 23:13:31 +0900

    API 도메인 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://13.125.195.239:8080",
+    API_URL: "https://api.ticketbell.store",
     KAKAO_CLIENT_ID: "a9e8beed55b5a4c283dc2a33e2024108",
   },
 };
