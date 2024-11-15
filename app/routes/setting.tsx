56a1cbef63fccf94e34807a53b3a83de9de34e1a
dev2820 <second28200@naver.com>
2024-11-15 23:00:13 +0900

    fix: 설정페이지 디자인 수정

--- app/routes/setting.tsx
+++ app/routes/setting.tsx
@@ -18,15 +18,15 @@ export default function Setting() {
   };
   return (
     <main className="h-screen w-full">
-      <header className="h-14 relative px-4 text-center">
+      <header className="h-14 relative px-4 text-center border-b">
         <button className="absolute left-4 top-3" onClick={handleGoBack}>
-          <ChevronLeft size={32} />
+          <ChevronLeft size={32} strokeWidth={1} />
         </button>
         <span className="text-xl my-auto absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold">
           설정
         </span>
       </header>
-      <div className="h-[calc(100%_-_64px)] px-4">
+      <div className="h-[calc(100%_-_64px)] px-4 py-4">
         <Button onClick={handleClickLogout}>로그아웃</Button>
       </div>
     </main>
