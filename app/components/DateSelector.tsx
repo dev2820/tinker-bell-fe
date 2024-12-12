65b9bb6c0c23e874f801547c4cf8cc2f4ba53be3
dev2820 <second28200@naver.com>
2024-12-13 00:01:35 +0900

    feat: calendar opacity와 transition 적용

--- app/components/DateSelector.tsx
+++ app/components/DateSelector.tsx
@@ -1,33 +1,44 @@
-import { ComponentProps } from "react";
-
 import { useDrag } from "@use-gesture/react";
 import { useSpring, animated, config } from "@react-spring/web";
 import { clamp } from "@/utils/clamp";
+import { isSameDay, isThisMonth, lastDayOfMonth, startOfMonth } from "date-fns";
 
-const height = 200;
+const CELL_HEIGHT = 48;
+const MAX_HEIGHT = CELL_HEIGHT * 6;
 
-type DateSelectorProps = ComponentProps<"div"> & {
+type DateSelectorProps = {
   currentDate: Date;
   onChangeDate?: (newDate: Date) => void;
 };
 export function DateSelector(props: DateSelectorProps) {
+  const { currentDate, onChangeDate } = props;
+
+  const daysInCalendar = getCalendarDays(currentDate);
+  const thisWeek = getWeek(daysInCalendar, currentDate);
   const [{ h }, api] = useSpring(() => ({
-    h: height,
+    h: MAX_HEIGHT,
     config: { tension: 10, friction: 26, mass: 0.3 },
   }));
-
+  const opacity = h.to((v) => {
+    return (v - CELL_HEIGHT) / (MAX_HEIGHT - CELL_HEIGHT);
+  });
+  const transitionY = h.to((v) => {
+    const startY = -thisWeek * CELL_HEIGHT;
+    const progress = (v - CELL_HEIGHT) / (MAX_HEIGHT - CELL_HEIGHT);
+    const y = startY * (1 - progress);
+    // thisWeek이 1이면
+    return y;
+  });
   const open = ({ canceled }: { canceled: boolean }) => {
-    // when cancel is true, it means that the user passed the upwards threshold
-    // so we change the spring config to create a nice wobbly effect
     api.start({
-      h: 50,
+      h: CELL_HEIGHT,
       immediate: false,
       config: canceled ? config.wobbly : config.stiff,
     });
   };
   const close = (velocity = 0) => {
     api.start({
-      h: height,
+      h: MAX_HEIGHT,
       immediate: false,
       config: { ...config.stiff, velocity },
     });
@@ -45,11 +56,14 @@ export function DateSelector(props: DateSelectorProps) {
       if (oh < -70) cancel();
 
       if (last) {
-        oh > height * 0.5 || (vh > 0.5 && dh > 0)
+        oh > MAX_HEIGHT * 0.5 || (vh > 0.5 && dh > 0)
           ? close(vh)
           : open({ canceled });
       } else {
-        api.start({ h: clamp(oh, 50, 200), immediate: true });
+        api.start({
+          h: clamp(oh, CELL_HEIGHT, MAX_HEIGHT),
+          immediate: true,
+        });
       }
     },
     {
@@ -59,12 +73,68 @@ export function DateSelector(props: DateSelectorProps) {
       rubberband: true,
     }
   );
+
   return (
     <animated.div
       {...bind()}
-      style={{ height: h, touchAction: "none", background: "red" }}
+      style={{
+        height: h,
+        touchAction: "none",
+        overflow: "hidden",
+        borderWidth: 1,
+      }}
     >
-      Hello
+      <animated.div
+        style={{ y: transitionY }}
+        className="grid grid-cols-7 place-items-center"
+      >
+        {daysInCalendar.map((date) => (
+          <animated.div
+            className="h-12"
+            style={{
+              color:
+                date.getMonth() === currentDate.getMonth()
+                  ? "#111111"
+                  : "#cccccc",
+              opacity: getWeek(daysInCalendar, date) === thisWeek ? 1 : opacity,
+            }}
+            key={date.toISOString()}
+          >
+            {date.getDate()}
+          </animated.div>
+        ))}
+      </animated.div>
     </animated.div>
   );
 }
+
+const getCalendarDays = (date: Date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const firstDate = startOfMonth(date);
+  const lastDate = lastDayOfMonth(date);
+
+  const days = [];
+
+  // 지난 달
+  for (let i = firstDate.getDay() - 1; i >= 0; i--) {
+    days.push(new Date(year, month, -i));
+  }
+
+  // 이번 달
+  for (let i = 1; i <= lastDate.getDate(); i++) {
+    days.push(new Date(year, month, i));
+  }
+
+  // 다음 달
+  const nextMonthDays = 42 - days.length; // 42 cells for 6 rows (7 columns)
+  for (let i = 1; i <= nextMonthDays; i++) {
+    days.push(new Date(year, month + 1, i));
+  }
+
+  return days;
+};
+
+const getWeek = (days: Date[], targetDate: Date) => {
+  return Math.floor(days.findIndex((d) => isSameDay(targetDate, d)) / 7);
+};
