ccbf836d7a32c678cab35eb403ac7768bfef1e9a
dev2820 <second28200@naver.com>
2024-12-26 09:37:35 +0900

    refactor: replace forward ref to ref as props

--- app/components/DateSelector.tsx
+++ app/components/DateSelector.tsx
@@ -2,7 +2,7 @@ import { useDrag } from "@use-gesture/react";
 import { useSpring, animated, config } from "@react-spring/web";
 import { clamp } from "@/utils/clamp";
 import { addMonths, addWeeks, isSameDay, isSaturday, isSunday } from "date-fns";
-import { forwardRef, useCallback, useEffect, useState } from "react";
+import { ComponentProps, useCallback, useEffect, useState } from "react";
 import { Swiper, SwiperItem } from "@/components/ui/Swiper";
 import { DailyTodoList } from "./todo/DailyTodoList";
 import { useCurrentDateStore } from "@/stores/current-date";
@@ -17,218 +17,214 @@ import { useModeStore } from "@/stores/mode";
 const CELL_HEIGHT = 56;
 const MAX_HEIGHT = CELL_HEIGHT * 6;
 
-type DateSelectorProps = {
-  className?: string;
+type DateSelectorProps = ComponentProps<"div"> & {
   height: number;
   width: number;
 };
-export const DateSelector = forwardRef<HTMLDivElement, DateSelectorProps>(
-  function (props, ref) {
-    const { className, width, height } = props;
-    const { currentDate, changeCurrentDate } = useCurrentDateStore(
-      useShallow((state) => ({ ...state }))
-    );
-    const { isReorderMode, onReorderMode, offReorderMode } = useModeStore();
-    const daysInCalendar = getCalendarDays(currentDate);
-    const thisWeek = getWeek(daysInCalendar, currentDate);
-    const [isDragging, setIsDragging] = useState<boolean>(false);
-    const [{ h }, api] = useSpring(() => ({
+export function DateSelector(props: DateSelectorProps) {
+  const { className, width, height, ref } = props;
+  const { currentDate, changeCurrentDate } = useCurrentDateStore(
+    useShallow((state) => ({ ...state }))
+  );
+  const { isReorderMode, onReorderMode, offReorderMode } = useModeStore();
+  const daysInCalendar = getCalendarDays(currentDate);
+  const thisWeek = getWeek(daysInCalendar, currentDate);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [{ h }, api] = useSpring(() => ({
+    h: CELL_HEIGHT,
+    config: { tension: 10, friction: 26, mass: 0.3 },
+  }));
+  const todoListHeight = h.to((v) => Math.max(height - v, 0));
+  const progress = h.to((v) =>
+    clamp((v - CELL_HEIGHT) / (MAX_HEIGHT - CELL_HEIGHT), 0, 1)
+  );
+  const transitionY = progress.to((v) => {
+    const startY = -thisWeek * CELL_HEIGHT;
+    const y = startY * (1 - v);
+    return y;
+  });
+  const preventEventInMotion = h.to((v) => {
+    if (v !== CELL_HEIGHT && v !== MAX_HEIGHT && isDragging) return "none";
+    return "auto";
+  });
+  const open = ({ canceled }: { canceled: boolean }) => {
+    api.start({
       h: CELL_HEIGHT,
-      config: { tension: 10, friction: 26, mass: 0.3 },
-    }));
-    const todoListHeight = h.to((v) => Math.max(height - v, 0));
-    const progress = h.to((v) =>
-      clamp((v - CELL_HEIGHT) / (MAX_HEIGHT - CELL_HEIGHT), 0, 1)
-    );
-    const transitionY = progress.to((v) => {
-      const startY = -thisWeek * CELL_HEIGHT;
-      const y = startY * (1 - v);
-      return y;
+      immediate: false,
+      config: canceled ? config.wobbly : config.stiff,
     });
-    const preventEventInMotion = h.to((v) => {
-      if (v !== CELL_HEIGHT && v !== MAX_HEIGHT && isDragging) return "none";
-      return "auto";
+  };
+  const close = (velocity = 0) => {
+    api.start({
+      h: MAX_HEIGHT,
+      immediate: false,
+      config: { ...config.stiff, velocity },
     });
-    const open = ({ canceled }: { canceled: boolean }) => {
-      api.start({
-        h: CELL_HEIGHT,
-        immediate: false,
-        config: canceled ? config.wobbly : config.stiff,
-      });
-    };
-    const close = (velocity = 0) => {
-      api.start({
-        h: MAX_HEIGHT,
-        immediate: false,
-        config: { ...config.stiff, velocity },
-      });
-    };
+  };
 
-    const bind = useDrag(
-      ({
-        last,
-        velocity: [, vh],
-        direction: [, dh],
-        offset: [, oh],
-        cancel,
-        canceled,
-      }) => {
-        if (oh < -70) {
-          cancel();
-          setIsDragging(false);
-        }
-
-        if (last) {
-          oh > MAX_HEIGHT * 0.9 || (vh > 0.15 && dh > 0)
-            ? close(vh)
-            : open({ canceled });
-          setIsDragging(false);
-        } else {
-          api.start({
-            h: clamp(oh, CELL_HEIGHT, MAX_HEIGHT),
-            immediate: true,
-          });
-          setIsDragging(true);
-        }
-      },
-      {
-        from: () => [0, h.get()],
-        filterTaps: true,
-        threshold: 20,
-        bounds: { top: 0 },
-        rubberband: true,
-        axis: "lock",
+  const bind = useDrag(
+    ({
+      last,
+      velocity: [, vh],
+      direction: [, dh],
+      offset: [, oh],
+      cancel,
+      canceled,
+    }) => {
+      if (oh < -70) {
+        cancel();
+        setIsDragging(false);
       }
-    );
 
-    const handleClickDate = (date: Date) => {
-      changeCurrentDate(date);
-    };
+      if (last) {
+        oh > MAX_HEIGHT * 0.9 || (vh > 0.15 && dh > 0)
+          ? close(vh)
+          : open({ canceled });
+        setIsDragging(false);
+      } else {
+        api.start({
+          h: clamp(oh, CELL_HEIGHT, MAX_HEIGHT),
+          immediate: true,
+        });
+        setIsDragging(true);
+      }
+    },
+    {
+      from: () => [0, h.get()],
+      filterTaps: true,
+      threshold: 20,
+      bounds: { top: 0 },
+      rubberband: true,
+      axis: "lock",
+    }
+  );
 
-    const handleChangeWeek = useCallback(
-      (direct: number) => {
-        changeCurrentDate(
-          addWeeks(
-            useCurrentDateStore.getState().currentDate,
-            direct > 0 ? 1 : -1
-          )
-        );
-      },
-      [changeCurrentDate]
-    );
-    const handleChangeMonth = useCallback(
-      (direct: number) => {
-        changeCurrentDate(
-          addMonths(
-            useCurrentDateStore.getState().currentDate,
-            direct > 0 ? 1 : -1
-          )
-        );
-      },
-      [changeCurrentDate]
-    );
+  const handleClickDate = (date: Date) => {
+    changeCurrentDate(date);
+  };
 
-    const handleClickOnReorderMode = () => {
-      onReorderMode();
-    };
+  const handleChangeWeek = useCallback(
+    (direct: number) => {
+      changeCurrentDate(
+        addWeeks(
+          useCurrentDateStore.getState().currentDate,
+          direct > 0 ? 1 : -1
+        )
+      );
+    },
+    [changeCurrentDate]
+  );
+  const handleChangeMonth = useCallback(
+    (direct: number) => {
+      changeCurrentDate(
+        addMonths(
+          useCurrentDateStore.getState().currentDate,
+          direct > 0 ? 1 : -1
+        )
+      );
+    },
+    [changeCurrentDate]
+  );
 
-    useEffect(() => {
-      offReorderMode();
-    }, [currentDate, offReorderMode]);
+  const handleClickOnReorderMode = () => {
+    onReorderMode();
+  };
 
-    return (
-      <div
-        ref={ref}
-        className={cn("", className)}
-        style={{ height: `${height}px` }}
+  useEffect(() => {
+    offReorderMode();
+  }, [currentDate, offReorderMode]);
+
+  return (
+    <div
+      ref={ref}
+      className={cn("", className)}
+      style={{ height: `${height}px` }}
+    >
+      <animated.div
+        {...bind()}
+        className={cn("relative px-4")}
+        style={{
+          height: h,
+          touchAction: "none",
+          overflow: "hidden",
+          pointerEvents: preventEventInMotion,
+        }}
       >
+        {/**
+         * 주단위 표기
+         */}
         <animated.div
-          {...bind()}
-          className={cn("relative px-4")}
           style={{
-            height: h,
-            touchAction: "none",
-            overflow: "hidden",
-            pointerEvents: preventEventInMotion,
+            opacity: progress.to((v) => 1 - v),
+            display: progress.to((v) => (v === 1 ? "none" : "block")),
           }}
         >
-          {/**
-           * 주단위 표기
-           */}
-          <animated.div
-            style={{
-              opacity: progress.to((v) => 1 - v),
-              display: progress.to((v) => (v === 1 ? "none" : "block")),
-            }}
-          >
-            <Swiper onChange={handleChangeWeek} width={width} className="h-16">
-              {[-1, 0, 1].map((idx) => (
-                <SwiperItem key={idx} index={idx}>
-                  <Weeks
-                    index={idx}
-                    currentDate={currentDate}
-                    onClickDate={handleClickDate}
-                  />
-                </SwiperItem>
-              ))}
-            </Swiper>
-          </animated.div>
-          {/**
-           * 월단위 표기
-           */}
-          <animated.div
-            className="absolute top-0 w-full"
-            style={{
-              opacity: progress,
-              y: transitionY,
-              display: progress.to((v) => (v === 0 ? "none" : "block")),
-            }}
-          >
-            <Swiper onChange={handleChangeMonth} width={width}>
-              {[-1, 0, 1].map((idx) => (
-                <SwiperItem key={idx} index={idx}>
-                  <Months
-                    currentDate={currentDate}
-                    index={idx}
-                    onClickDate={handleClickDate}
-                  />
-                </SwiperItem>
-              ))}
-            </Swiper>
-          </animated.div>
+          <Swiper onChange={handleChangeWeek} width={width} className="h-16">
+            {[-1, 0, 1].map((idx) => (
+              <SwiperItem key={idx} index={idx}>
+                <Weeks
+                  index={idx}
+                  currentDate={currentDate}
+                  onClickDate={handleClickDate}
+                />
+              </SwiperItem>
+            ))}
+          </Swiper>
         </animated.div>
+        {/**
+         * 월단위 표기
+         */}
         <animated.div
-          className="pt-4 w-full flex flex-col justify-center place-items-center overflow-y-hidden"
-          style={{ height: todoListHeight }}
+          className="absolute top-0 w-full"
+          style={{
+            opacity: progress,
+            y: transitionY,
+            display: progress.to((v) => (v === 0 ? "none" : "block")),
+          }}
         >
-          <div className="w-full h-full">
-            <div className="h-8 flex flex-row-reverse px-4">
-              {!isReorderMode && (
-                <Button
-                  theme="neutral"
-                  size="xs"
-                  variant="ghost"
-                  onClick={handleClickOnReorderMode}
-                  className="text-xs"
-                >
-                  순서 바꾸기
-                </Button>
-              )}
-            </div>
-            <div className="w-full h-[calc(100%_-_2rem)] overflow-y-auto">
-              <DailyTodoList
-                className="w-full pb-20"
-                currentDate={currentDate}
-                reorderMode={isReorderMode}
-              ></DailyTodoList>
-            </div>
-          </div>
+          <Swiper onChange={handleChangeMonth} width={width}>
+            {[-1, 0, 1].map((idx) => (
+              <SwiperItem key={idx} index={idx}>
+                <Months
+                  currentDate={currentDate}
+                  index={idx}
+                  onClickDate={handleClickDate}
+                />
+              </SwiperItem>
+            ))}
+          </Swiper>
         </animated.div>
-      </div>
-    );
-  }
-);
-DateSelector.displayName = "DateSelector";
+      </animated.div>
+      <animated.div
+        className="pt-4 w-full flex flex-col justify-center place-items-center overflow-y-hidden"
+        style={{ height: todoListHeight }}
+      >
+        <div className="w-full h-full">
+          <div className="h-8 flex flex-row-reverse px-4">
+            {!isReorderMode && (
+              <Button
+                theme="neutral"
+                size="xs"
+                variant="ghost"
+                onClick={handleClickOnReorderMode}
+                className="text-xs"
+              >
+                순서 바꾸기
+              </Button>
+            )}
+          </div>
+          <div className="w-full h-[calc(100%_-_2rem)] overflow-y-auto">
+            <DailyTodoList
+              className="w-full pb-20"
+              currentDate={currentDate}
+              reorderMode={isReorderMode}
+            ></DailyTodoList>
+          </div>
+        </div>
+      </animated.div>
+    </div>
+  );
+}
 
 const getWeek = (days: Date[], targetDate: Date) => {
   return Math.floor(days.findIndex((d) => isSameDay(targetDate, d)) / 7);
--- app/components/todo/TodoTitleTextarea.tsx
+++ app/components/todo/TodoTitleTextarea.tsx
@@ -1,13 +1,9 @@
 import { cx } from "@/utils/cx";
-import { forwardRef } from "react";
 import TextareaAutosize, {
   type TextareaAutosizeProps,
 } from "react-textarea-autosize";
 
-export const TodoTitleTextarea = forwardRef<
-  HTMLTextAreaElement,
-  TextareaAutosizeProps
->(function (props: TextareaAutosizeProps, ref) {
+export function TodoTitleTextarea(props: TextareaAutosizeProps) {
   const { className, ...rest } = props;
 
   return (
@@ -16,11 +12,9 @@ export const TodoTitleTextarea = forwardRef<
         "resize-none rounded-md p-2 border bg-white text-md focus:border-primary outline-primary border-gray-300",
         className
       )}
-      ref={ref}
       minRows={1}
       maxRows={3}
       {...rest}
     />
   );
-});
-TodoTitleTextarea.displayName = "TodoTitleTextarea";
+}
