afc2f91300b996ad797996bbdb1ec66b8f67ca22
dev2820 <second28200@naver.com>
2024-12-26 12:03:04 +0900

    chore: react-router config에 vercel preset 추가

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/remix/vite";
 
 export default {
   ssr: true,
+  presets: [vercelPreset()],
 } satisfies Config;
