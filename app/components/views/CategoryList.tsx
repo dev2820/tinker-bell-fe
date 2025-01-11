2e7375e613db3e2a45da8018f4347d4fc9fbfcd4
dev2820 <second28200@naver.com>
2025-01-11 12:35:13 +0900

    fix: Category CRUD 구현

--- app/components/views/CategoryList.tsx
+++ app/components/views/CategoryList.tsx
@@ -1,22 +1,51 @@
+import { cn } from "@/lib/utils";
 import { type Category } from "@/types/category";
-import { ComponentProps } from "react";
+import { type ComponentProps, type MouseEvent } from "react";
 
 export type CategoryListProps = ComponentProps<"ul"> & {
   items: Category[];
+  onClickCategory?: (id: Category["id"]) => void;
 };
 export function CategoryList(props: CategoryListProps) {
-  const { items, ...rest } = props;
+  const { items, className, onClickCategory, ...rest } = props;
+
+  const handleClickCategoryItem = (e: MouseEvent<HTMLButtonElement>) => {
+    const id = Number(e.currentTarget.dataset["categoryId"]);
+    onClickCategory?.(id);
+  };
+
   return (
-    <ul {...rest}>
+    <ul className={cn(className)} {...rest}>
       {items.map((item) => (
-        <li key={item.id}>
-          <CategoryItem></CategoryItem>
+        <li key={item.id} className="border-b">
+          <div className="h-12 px-4">
+            <CategoryItem
+              category={item}
+              data-category-id={item.id}
+              onClick={handleClickCategoryItem}
+              className="h-full w-full"
+            ></CategoryItem>
+          </div>
         </li>
       ))}
     </ul>
   );
 }
 
-const CategoryItem = () => {
-  return <div>item</div>;
+type CategoryItemProps = ComponentProps<"button"> & { category: Category };
+const CategoryItem = (props: CategoryItemProps) => {
+  const { category, className, ...rest } = props;
+
+  return (
+    <button
+      className={cn(className, "flex flex-row place-items-center gap-2")}
+      {...rest}
+    >
+      <span
+        style={{ backgroundColor: category.color }}
+        className="size-5 rounded-full"
+      ></span>
+      {category.name}
+    </button>
+  );
 };
--- app/routes/setting_.category.tsx
+++ app/routes/setting_.category.tsx
@@ -1,10 +1,15 @@
 import type { Category } from "@/types/category";
 import { CategoryList } from "@/components/views/CategoryList";
 import { ToastProvider } from "@/contexts/toast";
-import { createCategory, fetchCategories } from "@/utils/api/category";
+import {
+  createCategory,
+  deleteCategory,
+  fetchCategories,
+  updateCategory,
+} from "@/utils/api/category";
 import { routerBack } from "@/utils/helper/app";
 import { useNavigate } from "@remix-run/react";
-import { ChevronLeft, PlusIcon, TagIcon } from "lucide-react";
+import { ChevronLeft, PlusIcon, TagIcon, Trash2Icon } from "lucide-react";
 import { ChangeEvent, useEffect, useState } from "react";
 import { FloatingButton } from "@/components/ui/FloatingButton";
 import {
@@ -14,6 +19,7 @@ import {
   Input,
   Portal,
   ColorPickerValueChangeDetails,
+  IconButton,
 } from "terra-design-system/react";
 import { cn } from "@/lib/utils";
 import { getRandomHexColor } from "@/utils/color";
@@ -22,12 +28,14 @@ import { useDisclosure } from "@/hooks/use-disclosure";
 const MAX_CATEGORY_LENGTH = 15;
 export default function Category() {
   const navigate = useNavigate();
-  const [isCreating, setIsCreating] = useState<boolean>(false);
-  const drawerHandler = useDisclosure();
-  const [categoryName, setCategoryName] = useState<string>("");
-  const [categoryColor, setCategoryColor] = useState<string>(
-    getRandomHexColor()
-  );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const createDrawerHandler = useDisclosure();
+  const modifyDrawerHandler = useDisclosure();
+  const [newCategory, setNewCategory] = useState<Omit<Category, "id">>({
+    name: "",
+    color: `${getRandomHexColor()}`,
+  });
+  const [modifyCategory, setModifyCategory] = useState<Category | undefined>();
   const handleGoBack = () => {
     routerBack(navigate);
   };
@@ -43,26 +51,113 @@ export default function Category() {
     updateCategories();
   }, []);
 
-  const handleChangeCategory = (e: ChangeEvent<HTMLInputElement>) => {
-    setCategoryName(e.currentTarget.value.slice(0, 15));
+  const handleChangeNewCategoryName = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.currentTarget.value.slice(0, 15);
+    setNewCategory({
+      ...newCategory,
+      name: name,
+    });
   };
-  const handleChangeColor = (details: ColorPickerValueChangeDetails) => {
-    setCategoryColor(details.value.toString("hex"));
+  const handleChangeNewCategoryColor = (
+    details: ColorPickerValueChangeDetails
+  ) => {
+    const color = details.value.toString("hex");
+    setNewCategory({
+      ...newCategory,
+      color: color,
+    });
   };
   const handleClickAddCategory = async () => {
-    setIsCreating(true);
+    setIsLoading(true);
     try {
-      await createCategory({
-        name: categoryName,
-        color: categoryColor,
+      await createCategory(newCategory);
+      await updateCategories();
+      createDrawerHandler.onClose();
+      setNewCategory({
+        name: "",
+        color: getRandomHexColor(),
       });
-      drawerHandler.onClose();
     } catch (err) {
       /**
        * TODO: 에러에 따른 Toast 출력
        */
     } finally {
-      setIsCreating(false);
+      setIsLoading(false);
+    }
+  };
+
+  const handleClickCategory = (id: Category["id"]) => {
+    const category = categories.find((c) => c.id === id);
+    if (!category) {
+      return;
+    }
+    setModifyCategory({ ...category });
+    modifyDrawerHandler.onOpen();
+  };
+  const handleChangeModifyCategoryName = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.currentTarget.value.slice(0, 15);
+    if (!modifyCategory) {
+      return;
+    }
+    setModifyCategory({
+      ...modifyCategory,
+      name: name,
+    });
+  };
+  const handleChangeModifyCategoryColor = (
+    details: ColorPickerValueChangeDetails
+  ) => {
+    const color = details.value.toString("hex");
+    if (!modifyCategory) {
+      return;
+    }
+    setModifyCategory({
+      ...modifyCategory,
+      color: color,
+    });
+  };
+  const handleClickModifyCategory = async () => {
+    setIsLoading(true);
+    try {
+      if (!modifyCategory) {
+        return;
+      }
+      await updateCategory(modifyCategory);
+      await updateCategories();
+      modifyDrawerHandler.onClose();
+      setModifyCategory(undefined);
+    } catch (err) {
+      /**
+       * TODO: 에러에 따른 Toast 출력
+       */
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleClickPlusCategoryButton = () => {
+    setNewCategory({
+      ...newCategory,
+      color: getRandomHexColor(),
+    });
+    console.log("?");
+    createDrawerHandler.onOpen();
+  };
+
+  const handleDeleteCategory = async () => {
+    if (!modifyCategory) {
+      return;
+    }
+
+    try {
+      await deleteCategory({ id: modifyCategory.id });
+      await updateCategories();
+    } catch (err) {
+      /**
+       * TODO: handle error
+       */
+    } finally {
+      modifyDrawerHandler.onClose();
     }
   };
 
@@ -77,20 +172,26 @@ export default function Category() {
             카테고리
           </span>
         </header>
-        <div className="h-[calc(100%_-_64px)] px-4 py-4">
-          <CategoryList items={categories} />
+        <div className="h-[calc(100%_-_64px)]">
+          <CategoryList
+            items={categories}
+            onClickCategory={handleClickCategory}
+          />
+          <FloatingButton
+            variant="filled"
+            theme="primary"
+            shape="round"
+            onClick={handleClickPlusCategoryButton}
+          >
+            <PlusIcon size={32} />
+          </FloatingButton>
           <Drawer.Root
             variant="bottom"
-            open={drawerHandler.isOpen}
+            open={createDrawerHandler.isOpen}
             onOpenChange={(e) => {
-              drawerHandler.change(e.open);
+              createDrawerHandler.change(e.open);
             }}
           >
-            <Drawer.Trigger asChild>
-              <FloatingButton variant="filled" theme="primary" shape="round">
-                <PlusIcon size={32} />
-              </FloatingButton>
-            </Drawer.Trigger>
             <Portal>
               <Drawer.Backdrop />
               <Drawer.Positioner>
@@ -106,18 +207,18 @@ export default function Category() {
                       <TagIcon size={20} className="text-gray-300 flex-none" />
                       <input
                         className="w-full flex-1 text-gray-800 focus:outline-none text-md"
-                        onChange={handleChangeCategory}
-                        value={categoryName}
+                        onChange={handleChangeNewCategoryName}
+                        value={newCategory.name}
                         placeholder="카테고리 이름 (최대 15자)"
                       />
                       <small className="text-gray-300">
-                        {categoryName.length}/{MAX_CATEGORY_LENGTH}
+                        {newCategory.name.length}/{MAX_CATEGORY_LENGTH}
                       </small>
                     </div>
                     <hr />
                     <ColorPicker.Root
-                      value={categoryColor}
-                      onValueChange={handleChangeColor}
+                      value={newCategory.color}
+                      onValueChange={handleChangeNewCategoryColor}
                     >
                       <ColorPicker.Context>
                         {(api) => (
@@ -187,10 +288,132 @@ export default function Category() {
                     <Button
                       theme="primary"
                       className="w-full"
-                      loading={isCreating}
+                      loading={isLoading}
                       onClick={handleClickAddCategory}
                     >
                       추가
+                    </Button>
+                  </Drawer.Footer>
+                </Drawer.Content>
+              </Drawer.Positioner>
+            </Portal>
+          </Drawer.Root>
+          <Drawer.Root
+            variant="bottom"
+            open={modifyDrawerHandler.isOpen}
+            onOpenChange={(e) => {
+              modifyDrawerHandler.change(e.open);
+            }}
+            trapFocus={false}
+            unmountOnExit
+          >
+            <Portal>
+              <Drawer.Backdrop />
+              <Drawer.Positioner>
+                <Drawer.Content className="rounded-t-2xl">
+                  <Drawer.Header>
+                    <h2 className="text-xl h-9 leading-[36px]">
+                      카테고리 수정
+                    </h2>
+                    <IconButton
+                      onClick={handleDeleteCategory}
+                      size="sm"
+                      className="absolute right-4"
+                    >
+                      <Trash2Icon size={16} />
+                    </IconButton>
+                  </Drawer.Header>
+                  <Drawer.Body>
+                    <div className="relative flex flex-row gap-2 place-items-center py-2">
+                      <TagIcon size={20} className="text-gray-300 flex-none" />
+                      <input
+                        className="w-full flex-1 text-gray-800 focus:outline-none text-md"
+                        onChange={handleChangeModifyCategoryName}
+                        value={modifyCategory?.name ?? ""}
+                        placeholder="카테고리 이름 (최대 15자)"
+                      />
+                      <small className="text-gray-300">
+                        {modifyCategory?.name.length}/{MAX_CATEGORY_LENGTH}
+                      </small>
+                    </div>
+                    <hr />
+                    <ColorPicker.Root
+                      value={modifyCategory?.color ?? "#000000"}
+                      onValueChange={handleChangeModifyCategoryColor}
+                      unmountOnExit
+                    >
+                      <ColorPicker.Context>
+                        {(api) => (
+                          <>
+                            <ColorPicker.Control>
+                              <ColorPicker.Trigger asChild>
+                                <button className="w-full flex flex-row gap-2 place-items-center py-2 text-gray-800">
+                                  <ColorPicker.Swatch
+                                    className="rounded-full size-5"
+                                    value={api.value}
+                                  />
+                                  <span className="select-none">
+                                    {api.value.toString("hex")}
+                                  </span>
+                                </button>
+                              </ColorPicker.Trigger>
+                            </ColorPicker.Control>
+                            <ColorPicker.Positioner>
+                              <ColorPicker.Content>
+                                <div className="trds-flex trds-flex-col trds-gap-3">
+                                  <ColorPicker.Area>
+                                    <ColorPicker.AreaBackground />
+                                    <ColorPicker.AreaThumb />
+                                  </ColorPicker.Area>
+                                  <div className="trds-flex trds-flex-row trds-gap-3">
+                                    <div
+                                      className={cn(
+                                        "trds-flex trds-flex-col trds-gap-2 trds-flex-1"
+                                      )}
+                                    >
+                                      <ColorPicker.ChannelSlider channel="hue">
+                                        <ColorPicker.ChannelSliderTrack />
+                                        <ColorPicker.ChannelSliderThumb />
+                                      </ColorPicker.ChannelSlider>
+                                    </div>
+                                  </div>
+                                  <div className="trds-flex trds-gap-2 trds-flex-1">
+                                    <ColorPicker.ChannelInput
+                                      channel="hex"
+                                      className="w-full"
+                                      asChild
+                                    >
+                                      <Input />
+                                    </ColorPicker.ChannelInput>
+                                  </div>
+                                  <div className="trds-flex trds-flex-col trds-gap-1.5">
+                                    <ColorPicker.SwatchGroup>
+                                      {presets.map((color, id) => (
+                                        <ColorPicker.SwatchTrigger
+                                          key={id}
+                                          value={color}
+                                        >
+                                          <ColorPicker.Swatch value={color} />
+                                        </ColorPicker.SwatchTrigger>
+                                      ))}
+                                    </ColorPicker.SwatchGroup>
+                                  </div>
+                                </div>
+                              </ColorPicker.Content>
+                            </ColorPicker.Positioner>
+                          </>
+                        )}
+                      </ColorPicker.Context>
+                    </ColorPicker.Root>
+                  </Drawer.Body>
+                  <Drawer.Footer>
+                    <Button
+                      theme="primary"
+                      className="w-full"
+                      loading={isLoading}
+                      onClick={handleClickModifyCategory}
+                    >
+                      수정
                     </Button>
                   </Drawer.Footer>
                 </Drawer.Content>
