e6130ecc29f6a4aa3e202eb873239d2cc4ef6deb
dev2820 <second28200@naver.com>
2025-02-04 22:55:47 +0900

    feat: category 생성에 성공하면 반환받은 결과로 setQuery 수행

--- app/hooks/category/use-create-category.ts
+++ app/hooks/category/use-create-category.ts
@@ -1,6 +1,8 @@
 import * as CategoryAPI from "@/utils/api/category";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CATEGORY_QUERY_KEY } from "./query-key";
+import { toCategory } from "@/utils/helper/category";
+import { Category } from "@/types/category";
 
 export function useCreateCategory() {
   const queryClient = useQueryClient();
@@ -8,8 +10,14 @@ export function useCreateCategory() {
   return useMutation({
     mutationKey: CATEGORY_QUERY_KEY,
     mutationFn: CategoryAPI.createCategory,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: CATEGORY_QUERY_KEY });
+    onSuccess: (data) => {
+      const updatedCategory = toCategory(data);
+      queryClient.setQueryData<Category[]>(CATEGORY_QUERY_KEY, (categories) => {
+        if (!categories) return categories;
+
+        return [updatedCategory, ...categories];
+      });
+      // queryClient.invalidateQueries({ queryKey: CATEGORY_QUERY_KEY });
     },
     onError: (error) => {
       console.error("Error create category", error);
