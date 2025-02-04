d68e89a3668da44ddc01f85fa0351f812d83e5b2
dev2820 <second28200@naver.com>
2025-02-04 22:37:16 +0900

    fix: update category에 낙관적 업데이트 적용

--- app/hooks/category/use-update-category.ts
+++ app/hooks/category/use-update-category.ts
@@ -1,6 +1,7 @@
 import * as CategoryAPI from "@/utils/api/category";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CATEGORY_QUERY_KEY } from "./query-key";
+import { Category } from "@/types/category";
 
 export function useUpdateCategory() {
   const queryClient = useQueryClient();
@@ -8,11 +9,31 @@ export function useUpdateCategory() {
   return useMutation({
     mutationKey: CATEGORY_QUERY_KEY,
     mutationFn: CategoryAPI.updateCategory,
+    onMutate: (payload) => {
+      const previousCategories =
+        queryClient.getQueryData<Category[]>(CATEGORY_QUERY_KEY);
+
+      queryClient.setQueryData<Category[]>(CATEGORY_QUERY_KEY, (categories) => {
+        if (!categories) return categories;
+
+        const targetIndex = categories.findIndex((c) => c.id === payload.id);
+        const oldCategory = categories[targetIndex];
+
+        return categories.toSpliced(targetIndex, 1, {
+          ...oldCategory,
+          ...payload,
+        });
+      });
+
+      return { categories: previousCategories };
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: CATEGORY_QUERY_KEY });
     },
-    onError: (error) => {
-      console.error("Error update category", error);
+    onError: (_, __, context) => {
+      if (context?.categories) {
+        queryClient.setQueryData(CATEGORY_QUERY_KEY, context.categories);
+      }
     },
   });
 }
