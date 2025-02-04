548651ca5db0f0081e1531db862820e3ced606b0
dev2820 <second28200@naver.com>
2025-02-04 22:39:15 +0900

    feat: useDeleteCategory에 낙관적 업데이트 적용

--- app/hooks/category/use-delete-category.ts
+++ app/hooks/category/use-delete-category.ts
@@ -1,6 +1,7 @@
 import * as CategoryAPI from "@/utils/api/category";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CATEGORY_QUERY_KEY } from "./query-key";
+import { Category } from "@/types/category";
 
 export function useDeleteCategory() {
   const queryClient = useQueryClient();
@@ -8,11 +9,25 @@ export function useDeleteCategory() {
   return useMutation({
     mutationKey: CATEGORY_QUERY_KEY,
     mutationFn: CategoryAPI.deleteCategory,
+    onMutate: (payload) => {
+      const previousCategories =
+        queryClient.getQueryData<Category[]>(CATEGORY_QUERY_KEY);
+
+      queryClient.setQueryData<Category[]>(CATEGORY_QUERY_KEY, (categories) => {
+        if (!categories) return categories;
+
+        return categories.filter((c) => c.id !== payload.id);
+      });
+
+      return { categories: previousCategories };
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: CATEGORY_QUERY_KEY });
     },
-    onError: (error) => {
-      console.error("Error delete category", error);
+    onError: (_, __, context) => {
+      if (context?.categories) {
+        queryClient.setQueryData(CATEGORY_QUERY_KEY, context.categories);
+      }
     },
   });
 }
